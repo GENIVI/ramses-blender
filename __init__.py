fc44a267880ff841b9729a210115ff1dc90d821e
Violin Yanev <violin.yanev@gmail.com>
2019-08-11 15:02:34 +0200

    Fix missing import

--- __init__.py
+++ __init__.py
@@ -48,6 +48,7 @@ import bpy # NOTE: the bpy import must come below the module reload code
 import pathlib
 import os
 from ramses_export import debug_utils
+from ramses_export import utils
 from ramses_export.ramses_inspector import RamsesInspector
 from ramses_export.exporter import RamsesBlenderExporter
 from bpy_extras.io_utils import ExportHelper
